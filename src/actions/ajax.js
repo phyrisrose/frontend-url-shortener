792f12dcc7e1385a2820bd1c637cb5e2cc61cb5c
Anna <phyrisrose@gmail.com>
2022-05-08 12:16:05 -0600

    some cleanup in ajax

--- src/actions/ajax.js
+++ src/actions/ajax.js
@@ -1,18 +1,15 @@
 import { API } from "../constants";
 
-// Example POST method implementation:
 export async function postData(url = "", data = {}) {
-  // Default options are marked with *
   const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    method: "POST",
+    cache: "no-cache",
     headers: {
       "Content-Type": "application/json",
       "GB-Access-Token": API.KEY,
-      // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
   });
-  return response.json(); // parses JSON response into native JavaScript objects
+  return response.json();
 }
