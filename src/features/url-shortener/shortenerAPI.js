e919c608b43643b47dce0a67a480553cd5d87a3a
Anna <phyrisrose@gmail.com>
2022-05-09 11:48:50 -0600

    todo ~ todone

--- src/features/url-shortener/shortenerAPI.js
+++ src/features/url-shortener/shortenerAPI.js
@@ -1,7 +1,6 @@
 import { postData } from "../../app/ajax";
 import { API } from "../../constants";
 
-/** @todo does this need to be async? */
 export function shortenUrl(longUrl = "") {
   return postData(`${API.BASE}/links`, {
     url: longUrl,
