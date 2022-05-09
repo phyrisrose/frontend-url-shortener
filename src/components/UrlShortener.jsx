64c577568a7d95be1958e71534ef4bc428b26686
Anna <phyrisrose@gmail.com>
2022-05-08 18:27:08 -0600

    the shortner portion is starting to look decent

--- src/components/UrlShortener.jsx
+++ src/components/UrlShortener.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
 
 /** @todo move to actions */
 import { postData } from "../actions/ajax";
@@ -26,16 +26,23 @@ const UrlShortener = () => {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <TextField
-          id="long-url"
-          label="Url to shorten"
-          variant="outlined"
-          value={longUrl}
-          onChange={handleLongUrlChange}
-        />
-        <Button variant="contained" onClick={handleShorten}>
-          Shorten
-        </Button>
+        <Grid container spacing={2} style={{ alignItems: "center" }}>
+          <Grid item xs={8}>
+            <TextField
+              id="long-url"
+              label="Url to shorten"
+              variant="outlined"
+              value={longUrl}
+              onChange={handleLongUrlChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="contained" onClick={handleShorten}>
+              Shorten
+            </Button>{" "}
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
