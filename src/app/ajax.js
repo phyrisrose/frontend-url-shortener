69a264da03ca4b631ba8d70a9096b458fac3a849
Anna <phyrisrose@gmail.com>
2022-05-10 11:29:20 -0600

    stubbing out GET

--- src/app/ajax.js
+++ src/app/ajax.js
@@ -1,5 +1,20 @@
 import { API } from "../constants";
 
+/** @todo check that this works */
+export async function getData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "GET",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+      "GB-Access-Token": API.KEY,
+    },
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
 export async function postData(url = "", data = {}) {
   const response = await fetch(url, {
     method: "POST",
