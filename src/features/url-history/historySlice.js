edbb9f2ad34680a98a9d7ddd95b359d1e40cbbf8
Anna <phyrisrose@gmail.com>
2022-05-10 11:53:04 -0600

    writing out history reducer

---
+++ src/features/url-history/historySlice.js
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getHistory } from "./historyAPI";
+
+const initialState = {
+  urls: [
+    // {
+    //   short_url: "http://bely.me/mZ9G",
+    //   slug: "mZ9G",
+    //   url: "https://gb.com/shorty",
+    // },
+  ],
+  loading: false,
+};
+
+const getHistoryAsync = createAsyncThunk("history/get", async () => {
+  const response = await getHistory();
+  return response;
+});
+
+export const historySlice = createSlice({
+  name: "history",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getHistoryAsync.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getHistoryAsync.fulfilled, (state, action) => {
+        state.loading = false;
+        state.urls = action.payload;
+      });
+    /** @todo add error case */
+  },
+});
+
+export default historySlice.reducer;
