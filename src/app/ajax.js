376837d3d28d53ad7b3e6b6aa805c22e9c4535b3
Anna <phyrisrose@gmail.com>
2022-05-10 12:13:34 -0600

    a bit of cleanup in ajax.js

--- src/app/ajax.js
+++ src/app/ajax.js
@@ -1,14 +1,9 @@
 import { API } from "../constants";
 
-/**
- * @todo check that this works
- * specifically, get parameters
- */
 export async function getData(url = "") {
   const response = await fetch(url, {
     cache: "no-cache",
     headers: {
-      // "Content-Type": "application/json",
       "GB-Access-Token": API.KEY,
     },
     referrerPolicy: "no-referrer",
