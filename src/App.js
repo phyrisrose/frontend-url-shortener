f602fbee608cd0279e45fd86779107e549adf16d
Anna <phyrisrose@gmail.com>
2022-05-10 11:39:13 -0600

    putting together a component to call url history

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Container from "@mui/material/Container";
 
 import Layout from "./Layout";
 import { UrlShortener } from "./features/url-shortener";
+import { UrlHistory } from "./features/url-history";
 
 function App() {
   return (
     <Layout>
       <Container maxWidth="sm">
         <UrlShortener />
+        <UrlHistory />
       </Container>
     </Layout>
   );
---
+++ src/features/url-history/UrlHistory.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+
+const UrlHistory = () => {
+  <Card sx={{ minWidth: 275 }}>
+    <CardContent>Url history placeholder</CardContent>
+  </Card>;
+};
+
+export default UrlHistory;
