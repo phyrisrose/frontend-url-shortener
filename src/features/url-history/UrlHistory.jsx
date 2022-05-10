884ce2f0d5388cc02895b3d19aa6ac5eb8883dea
Anna <phyrisrose@gmail.com>
2022-05-10 15:36:56 -0600

    installing MUI icons, plus basic url setup

--- src/features/url-history/UrlHistory.jsx
+++ src/features/url-history/UrlHistory.jsx
@@ -3,6 +3,14 @@ import { useSelector, useDispatch } from "react-redux";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import FolderIcon from "@mui/icons-material/Folder";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import { getHistoryAsync } from "./historySlice";
 
@@ -25,6 +33,27 @@ const UrlHistory = () => {
         <Typography color="text.secondary" gutterBottom>
           Url History
         </Typography>
+        <List>
+          {urls.map((url) => (
+            <ListItem
+              secondaryAction={
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              }
+            >
+              <ListItemAvatar>
+                <Avatar>
+                  <FolderIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Single-line item"
+                secondary="Secondary text"
+              />
+            </ListItem>
+          ))}
+        </List>
       </CardContent>
     </Card>
   );
