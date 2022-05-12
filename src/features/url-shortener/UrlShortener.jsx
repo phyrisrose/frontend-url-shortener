77568fcdbc1df02b2e24812898d8258c773df2bb
Anna <phyrisrose@gmail.com>
2022-05-12 12:00:16 -0600

    setting the shortened link to be a link

--- src/features/url-shortener/UrlShortener.jsx
+++ src/features/url-shortener/UrlShortener.jsx
@@ -5,7 +5,7 @@ import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
 import { shortenAsync, clearShortUrl } from "./shortnenerSlice";
 import { getHistoryAsync } from "../url-history";
@@ -51,9 +51,14 @@ const UrlShortener = () => {
             </Button>
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="body" component="div">
+            <Link
+              href={shortUrl}
+              color="inherit"
+              target="_blank"
+              rel="noopener"
+            >
               {shortUrl}
-            </Typography>
+            </Link>
           </Grid>
         </Grid>
       </CardContent>
