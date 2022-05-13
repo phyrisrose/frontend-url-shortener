1c536f770c9924e4856f663bf2de67c933f3db4e
Anna <phyrisrose@gmail.com>
2022-05-13 10:55:33 -0600

    adding test utils jsx

---
+++ src/test/test-utils.jsx
@@ -0,0 +1,30 @@
+// test-utils.jsx
+import React from "react";
+import { render as rtlRender } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+// Import your own reducer
+import { shortenerReducer } from "../features/url-shortener";
+import { historyReducer } from "../features/url-history";
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({
+      reducer: { shortener: shortenerReducer, history: historyReducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from "@testing-library/react";
+// override render method
+export { render };
